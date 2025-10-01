f8f34c9bad7418c7142d7046ae93b19ac36dc8b5
teshan-nuwantha <teshannuwantha@gmail.com>
2025-10-01 22:32:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let apikey = "9aa978e061d24d0c80963554250110";
+
+function loadWeatherData(searchval){
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${searchval}`)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('root').innerHTML = `
+        <section class="bg-white rounded-2xl shadow-xl p-6 text-gray-800 flex flex-col items-center">
+            <img src="${data.current.condition.icon}" alt="weather icon" class="w-20 h-20">
+            <h2 class="text-2xl font-bold mt-2">${data.location.name}, ${data.location.country}</h2>
+            <p class="text-lg text-gray-600 mt-1">${data.current.condition.text}</p>
+            <p class="text-4xl font-extrabold mt-4">${data.current.temp_c}°C</p>
+        </section>`;
+        console.log(data);
+    })
+    .catch(err => {
+        document.getElementById('root').innerHTML = `
+        <p class="text-red-200 bg-red-600 p-3 rounded-lg"> Error: City not found!</p>`;
+    });
+}
+
+function searchByCity(){
+    let searchval = document.getElementById('txtSearchInput').value;
+    if(searchval.trim() !== ""){
+        loadWeatherData(searchval);
+    }
+}
+
+function getgeolocation(){
+    navigator.geolocation.getCurrentPosition(showposition);
+}
+
+function showposition(position){
+    loadWeatherData(position.coords.latitude + "," + position.coords.longitude);
+}
+
+getgeolocation();
